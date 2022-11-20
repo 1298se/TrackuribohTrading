f6f0b47f1ce4fa915aab5d09490af07c1847d14b
olive <oliversong7h@gmail.com>
2022-11-19 19:33:32 -0500

    skull emoji python dependencies

--- update_ygo_database.py
+++ update_ygo_database.py
@@ -1,7 +1,6 @@
 import os
 from datetime import datetime
 
-from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session as DBSession
@@ -15,7 +14,6 @@ from models.printing import Printing
 from models.rarity import Rarity
 from models.sku import Sku
 from repositories.tcgplayer_catalog_repository import TCGPlayerCatalogRepository
-from scripts import paginate
 from services.tcgplayer_catalog_service import TCGPlayerApiService
 
 load_dotenv()
@@ -65,6 +63,38 @@ def _fetch_cards_in_set(set_id: int) -> list:
     )
 
     return set_cards
+
+
+from concurrent.futures import as_completed, ThreadPoolExecutor
+from typing import Callable, Any
+
+MAX_PARALLEL_NETWORK_REQUESTS = 5
+PAGINATION_SIZE = 100
+
+
+def paginate(
+        total,
+        paginate_fn: Callable[[int, int], Any],
+        on_paginated: Callable[[Any], None],
+        num_parallel_requests=MAX_PARALLEL_NETWORK_REQUESTS,
+        pagination_size=PAGINATION_SIZE
+):
+    if total == 0:
+        return
+
+    batch_offset_increments = min(total, num_parallel_requests * pagination_size)
+
+    for batch_offset in range(0, total, batch_offset_increments):
+        with ThreadPoolExecutor(num_parallel_requests) as executor:
+            futures = [executor.submit(paginate_fn, cur_offset, pagination_size)
+                       for cur_offset in range(
+                    batch_offset,
+                    min(batch_offset + batch_offset_increments, total),
+                    pagination_size
+                )]
+
+            for future in futures:
+                on_paginated(future.result())
 
 
 def update_card_database():
