4f3ead2d5169732f2c91ca1ca152fbe2a31e6496
olive <oliversong7h@gmail.com>
2024-03-27 08:36:07 -0400

    revert this shit fml

--- tasks/fetch_card_listings.py
+++ tasks/fetch_card_listings.py
@@ -1,11 +1,7 @@
 import logging
 from collections import defaultdict
-from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
-from itertools import groupby
 from typing import List, Dict
-
-from sqlalchemy.orm import Session
 
 from models import db_sessionmaker, SKU, Condition
 # from models.sku_listing import SKUListing
@@ -14,70 +10,67 @@ from models.sku_listings_batch_aggregate_data import SKUListingsBatchAggregateDa
 from services.tcgplayer_listing_service import get_product_active_listings
 from tasks.log_runtime_decorator import log_runtime
 from tasks.custom_types import CardRequestData, SKUListingResponse
-from tasks.utils import paginateWithBackoff, split_into_segments
+from tasks.utils import paginateWithBackoff
 
 logger = logging.getLogger(__name__)
 
-NUM_WORKERS = 14
+session = db_sessionmaker()
 
 
 def _insert_listing_data(
-        session: Session,
         sku_listing_responses: List[SKUListingResponse],
         timestamp: datetime,
 ):
-    sku_listings = [
-        SKUListing.from_tcgplayer_response(listing_response, timestamp) for listing_response in sku_listing_responses
-    ]
+    sku_id_to_listing_response_dict: Dict[int, List[SKUListingResponse]] = defaultdict(list)
 
-    session.add_all(sku_listings)
+    for response in sku_listing_responses:
+        sku_id_to_listing_response_dict[int(response['productConditionId'])].append(response)
 
-    sku_listings_groupby = groupby(sku_listings, lambda listing: listing.sku_id)
+    sku_id_to_batch_aggregate_data_dict = {sku_id: SKUListingsBatchAggregateData(
+        sku_id=sku_id,
+        timestamp=timestamp,
+        lowest_listing_price=min(map(lambda response: response['price'] + response['sellerShippingPrice'], responses)),
+        total_listings_count=len(responses),
+        total_copies_count=int(sum(map(lambda response: response['quantity'], responses)))
+    ) for sku_id, responses in sku_id_to_listing_response_dict.items()}
 
-    for sku_id, listings in sku_listings_groupby:
-        listings = list(listings)
-
-        sku_id_to_batch_aggregate_data = SKUListingsBatchAggregateData(
-            sku_id=sku_id,
-            timestamp=timestamp,
-            lowest_listing_price=min([listing.price + listing.shipping_price for listing in listings]),
-            total_listings_count=len(listings),
-            total_copies_count=sum([listing.quantity for listing in listings])
-        )
-
-        session.add(sku_id_to_batch_aggregate_data)
+    session.add_all(sku_id_to_batch_aggregate_data_dict.values())
 
     session.commit()
 
+    sku_listings = map(
+        lambda response: SKUListing.from_tcgplayer_response(
+            response,
+            timestamp,
+        ),
+        sku_listing_responses,
+    )
 
-def download_product_listings(requests: List[CardRequestData], timestamp: datetime):
-    session = db_sessionmaker()
-
-    for request in requests:
-        listings = get_product_active_listings(request)
-
-        if not listings:
-            logger.info(f"no results found for {request}", request)
-
-        _insert_listing_data(session, listings, timestamp)
+    session.add_all(sku_listings)
 
 
 def fetch_card_listings(
-    requests: List[CardRequestData],
+    requests: list[CardRequestData],
 ):
     start_time = datetime.utcnow()
     logger.info(f'Fetching listings for {len(requests)} requests')
 
-    card_request_data_segments = split_into_segments(requests, NUM_WORKERS)
+    # This doesn't seem to have a rate limit...
+    paginateWithBackoff(
+        total=len(requests),
+        paginate_fn=lambda offset: get_product_active_listings(requests[offset]),
+        pagination_size=1,
+        on_paginated=lambda active_listings: _insert_listing_data(
+            active_listings,
+            start_time
+        )
+    )
 
-    with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
-        [executor.submit(download_product_listings, segment, start_time) for segment in card_request_data_segments]
+    session.commit()
 
 
 @log_runtime
 def fetch_all_near_mint_card_listing_data():
-    session = db_sessionmaker()
-
     """
         Fetches all near mint listings for given printings. Currently this isn't job isn't affected by rate limiting.
 
@@ -93,13 +86,16 @@ def fetch_all_near_mint_card_listing_data():
     for key, value in card_id_to_skus_list:
         card_id_to_skus_dict[key].append(value)
 
-    near_mint_skus_requests = [
-        CardRequestData(
-            product_id=skus[0].card_id,
-            conditions=[near_mint_condition.name],
-            printings=[sku.printing.name for sku in skus],
-        ) for skus in card_id_to_skus_dict.values()
-    ]
+    near_mint_skus_requests = list(
+        map(
+            lambda skus: CardRequestData(
+                product_id=skus[0].card_id,
+                conditions=[near_mint_condition.name],
+                printings=[sku.printing.name for sku in skus],
+            ),
+            card_id_to_skus_dict.values(),
+        )
+    )
 
     fetch_card_listings(near_mint_skus_requests)
 
