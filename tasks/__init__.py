4bdf08e9141590a66f6e92724acced07c37c1b1e
olive <oliversong7h@gmail.com>
2024-04-03 21:50:57 -0400

    fix sentry

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -7,7 +7,7 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 sentry_sdk.init(
-    dsn="https://b4c75638f0b0c1048390ff7a3b0fb352@o4506209812873216.ingest.sentry.io/4506209838366720",
+    dsn="https://10a229c7bc4d2953f655cca7add05f6f@o4506209812873216.ingest.us.sentry.io/4507025756061696",
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     traces_sample_rate=1.0,
